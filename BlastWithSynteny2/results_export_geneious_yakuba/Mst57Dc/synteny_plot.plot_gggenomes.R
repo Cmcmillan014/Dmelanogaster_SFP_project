302afe6294b4a43d78d9dea1c606df658642ee1a
colinmcmillan <comcmill@syr.edu>
2026-05-21 12:27:22 -0400

    Blast with Synteny results between Dmel and Dyak. Complete for all SFPs

---
+++ BlastWithSynteny2/results_export_geneious_yakuba/Mst57Dc/synteny_plot.plot_gggenomes.R
@@ -0,0 +1,20 @@
+
+    library(tibble)
+    library(gggenomes)
+
+    # Load input files
+    seqs <- readr::read_tsv("results_export_geneious/Mst57Dc/gggenomes_seqs.tsv")
+    genes <- readr::read_tsv("results_export_geneious/Mst57Dc/gggenomes_genes.tsv")
+    links <- readr::read_tsv("results_export_geneious/Mst57Dc/gggenomes_links.tsv")
+
+    # Generate plot
+    p <- gggenomes(genes=genes, seqs=seqs, links=links) +
+        geom_link() +
+        geom_seq() +
+        geom_seq_label() +
+        geom_gene_tag(aes(label=gene_name), nudge_y=0.1, check_overlap = TRUE) +
+        geom_gene(aes(fill=gene_name), alpha = 0.3) 
+
+    # Save plot
+    ggplot2::ggsave("results_export_geneious/Mst57Dc/synteny_plot.pdf", plot=p, width=10, height=4)
+    
